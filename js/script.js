e7595b3b63bc223e2ebecb4cbe227fc47c174c6f
LaraRhiannon <35492529+LaraRhiannon@users.noreply.github.com>
2018-04-09 22:22:41 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -51,52 +51,112 @@ $(function() {
 
 
 
-$(function() {
-  var scatterChart = Highcharts.chart('scatter', {
+var timedata = [
+  ["al","1"  ],
+  ["ad","1"  ],
+  ["ar","19"  ],
+  ["am","1"  ],
+  ["au","148"  ],
+  ["at","12"  ],
+  ["be","84"  ],
+  ["br","56"  ],
+  ["ca","110"  ],
+  ["cn","188"  ],
+  ["ci","1"  ],
+  ["co","8"  ],
+  ["cy","8"  ],
+  ["cz","10"  ],
+  ["do","2"  ],
+  ["ec","3"  ],
+  ["eg","3"  ],
+  ["es","218"  ],
+  ["ee","6"  ],
+  ["fi","26"  ],
+  ["fk","1"  ],
+  ["fr","546"  ],
+  ["fo","2"  ],
+  ["gb","35154"  ],
+  ["gg","8"  ],
+  ["gh","1"  ],
+  ["gi","4"  ],
+  ["gp","1"  ],
+  ["hk","106"  ],
+  ["hu","6"  ],
+  ["im","9"  ],
+  ["in","32"  ],
+  ["ie","345"  ],
+  ["is","13"  ],
+  ["il","8"  ],
+  ["it","382"  ],
+  ["je","14"  ],
+  ["jp","76"  ],
+  ["ke","10"  ],
+  ["kr","10"  ],
+  ["li","1"  ],
+  ["lt","6"  ],
+  ["lu","5"  ],
+  ["ma","6"  ],
+  ["mx","61"  ],
+  ["mk","2"  ],
+  ["mt","6"  ],
+  ["no","90"  ],
+  ["nz","40"  ],
+  ["pe","6"  ],
+  ["pl","62"  ],
+  ["qa","2"  ],
+  ["re","1"  ],
+  ["ro","8"  ],
+  ["ru","34"  ],
+  ["sm","1"  ],
+  ["sk","11"  ],
+  ["se","96"  ],
+  ["th","7"  ],
+  ["tl","1"  ],
+  ["tr","2"  ],
+  ["ua","5"  ],
+  ["um","1"  ],
+  ["us","655"  ],
+  ["ve","5"  ]
+]
+
+
+Highcharts.mapChart('mapdata', {
     chart: {
-      type: 'scatter',
+        map: 'custom/world-highres'
     },
-    title: {
-      text: 'scatterplot chart',
+     title: {
+        text: "Who runs the London Marathon?"
     },
-    xAxis: {
-      //categories: ['Apples', 'Bananas', 'Oranges'],
+
+    subtitle: {
+        text: 'People from all over the world race the London marathon.'
     },
-    yAxis: {
-      // title: {
-      //   text: 'Fruit eaten',
-      // },
+
+    mapNavigation: {
+        enabled: true,
+        buttonOptions: {
+            verticalAlign: 'bottom'
+        }
     },
-    series: [
-      {
-        data: [
-          [29.87, 29.87],
-          [24.27, 24.27],
-          [24.86, 24.86],
-          [23.16, 23.16],
-          [21.12, 21.12],
-          [24.64, 24.64],
-          [27.23, 27.23],
-          [28.48, 28.48],
-          [26.92, 26.92],
-          [32.1, 32.1],
-          [20.45, 20.45],
-          [18.36, 18.36],
-          [32.13, 32.13],
-          [27.67, 27.67],
-          [29.12, 29.12],
-          [25.77, 25.77],
-          [25.53, 25.53],
-          [29.69, 29.69],
-          [34.55, 34.55],
-          [32.67, 32.67],
-          [24.01, 24.01],
-          [14.62, 14.62],
-          [27.16, 27.16],
-          [25.63, 25.63],
-          [27.45, 27.45],
-        ],
-      },
-    ],
-  });
+
+    colorAxis: {
+            min: 1,
+            max: 35154,
+            type: 'logarithmic',
+            maxColor: '#b30000',
+            minColor: '#ffb3b3',
+        },
+
+    series: [{
+        data: timedata,
+        name: 'Time',
+        joinby: 'hc-key',
+        mapData: Highcharts.maps['custom/world'],
+        states: {
+            hover: {
+                color: '#cc0000'
+            }
+        },
+        
+    }]
 });
